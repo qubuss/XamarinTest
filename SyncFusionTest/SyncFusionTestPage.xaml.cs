a35e2b98081941d9a5f83fcf25d733e265bef12d
JakubFryga@simple.com.pl <JakubFryga@simple.com.pl>
2017-11-17 08:55:57 +0100

    Initial check-in of module SyncFusionTest

---
+++ SyncFusionTest/SyncFusionTestPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SyncFusionTest
+{
+    public partial class SyncFusionTestPage : ContentPage
+    {
+        public SyncFusionTestPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            IsBusy = false;
+            Done();
+        }
+
+        private async void Done(){
+            IsBusy = true;
+            await Task.Delay(5000);
+            IsBusy = false;
+        }
+    }
+}
